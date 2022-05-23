11e3738b28cf3e4ca984fbc06afef216355c58c9
KennyWong0701 <kennywong1266@gmail.com>
2022-05-23 16:36:44 -0600

    Taller 1

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Kenny Ramos Wong
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/kenny-ramos-wong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contacto
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
